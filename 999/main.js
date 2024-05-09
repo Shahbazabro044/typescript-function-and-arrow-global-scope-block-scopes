4e506a54ec6302c5720303e7332b358cffa25bc5
Shahbaz <167298370+Shahbazabro044@users.noreply.github.com>
2024-05-09 23:38:47 +0500

    Add files via upload

---
+++ 999/main.js
@@ -0,0 +1,53 @@
+"use strict";
+/*function Country():string{
+    console.log("pak")
+     return "aus"
+}
+const ans = Country()
+console.log(ans)*/
+/////////ARROW FUNCTION 
+/*const Hello = () => {
+    console.log(`hello from arrow function`)
+}
+Hello()*/
+// const isEven = (digit:number) =>{
+//     if(digit % 2  ==0){
+//       console.log("even")
+//     }else{
+//         console.log("odd")
+//     }
+// 
+// isEven(20)
+///////////return and singal statment 
+// const Name = () => "shahbaz "
+// const ans1 = Name()
+// console.log(ans1)
+// const digit = () => 12
+// const ans2 = digit()
+// console.log(ans2)
+// const ispass = () => 12
+// const ans3 = ispass()
+// console.log(ans3)
+// const calc = (digit:number , digit1:number , singh:string)=>{
+//    if(singh == "+"){
+//     console.log(digit + digit1)
+//    }
+//    else (singh == "-")
+//     console.log(digit - digit1) 
+// }
+// calc(10 , 15 ,"+")
+// calc(10 , 15 ,"-")
+/////////  GLOBALS AND LOCALS  SCOPS 
+const Pakarmy = "pakarmy";
+const kps = () => {
+    let kps = "KPS";
+    console.log(kps);
+    console.log(Pakarmy);
+};
+kps();
+const spf = () => {
+    let spf = "SFP";
+    console.log(spf);
+    console.log(Pakarmy);
+};
+spf();
